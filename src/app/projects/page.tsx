f7bbd5ae0aa18d04108cc1b15c279b9619fd2784
Jose Fernando Álvarez Romero <joseferalvarezromero@gmail.com>
2023-09-16 21:08:08 +0200

    routes created

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function page() {
+    return (
+        <>
+            <div>projects</div>
+            <button onClick={() => { console.log('hola') }}></button>
+        </>
+    )
+}
